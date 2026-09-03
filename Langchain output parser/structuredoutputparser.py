7611b598a29bf46afd1ee7de7a4ab667f8d04c43
Rishu <rishuchaudhary870@gmail.com>
2026-09-03 17:44:04 +0530

    type3 structuredop

---
+++ Langchain output parser/structuredoutputparser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    model="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+class FactsSchema(BaseModel):
+    fact_1: str = Field(description="Fact 1 about the topic")
+    fact_2: str = Field(description="Fact 2 about the topic")
+    fact_3: str = Field(description="Fact 3 about the topic")
+
+parser = PydanticOutputParser(pydantic_object=FactsSchema)
+
+template = PromptTemplate(
+    template='Give 3 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic': 'black hole'})
+
+print(result)
